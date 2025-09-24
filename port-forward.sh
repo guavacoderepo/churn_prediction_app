fae819d3d5d50f8cb39fc8c648f8bbf99ce7ce97
guavacoderepo <guavacodeplus@gmail.com>
2025-09-24 17:35:47 +0100

    folder restructure

--- port-forward.sh
+++
@@ -1,4 +0,0 @@
-kubectl port-forward svc/fastapi-service 8000:8000 -n churn-stack &
-kubectl port-forward svc/prometheus-operated 9090:9090 -n churn-stack &
-kubectl port-forward svc/prometheus-grafana 5050:80 -n churn-stack &
- 
